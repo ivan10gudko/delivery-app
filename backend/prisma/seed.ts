7467df74dac9b956ad2af9b650aafa925736e8be
Ivan Hudko <giv.10072007@gmail.com>
2026-03-26 22:28:18 +0200

    create seed prisma

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,19 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL, ssl: true });
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
-  // 1. Очищення бази перед посівом (опціонально)
   await prisma.orderItem.deleteMany();
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
   await prisma.shop.deleteMany();
 
-  // 2. Створення категорій (Middle Level) [cite: 89]
   const burgerCat = await prisma.category.create({ data: { name: 'Burgers' } });
   const drinkCat = await prisma.category.create({ data: { name: 'Drinks' } });
 
-  // 3. Створення магазинів із рейтингом [cite: 104]
   const mcDonny = await prisma.shop.create({
     data: { name: 'Mc Donny', rating: 4.8 }
   });
@@ -21,7 +22,6 @@ async function main() {
     data: { name: 'CFK', rating: 4.2 }
   });
 
-  // 4. Додавання товарів (Base Level) [cite: 52]
   await prisma.product.createMany({
     data: [
       { name: 'Big Big Burger', price: 15.5, shopId: mcDonny.id, categoryId: burgerCat.id },
